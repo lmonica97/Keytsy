d343cdce79ab54f0e11bec999578a26e087c2ad8
Monica Liang <monicaliang@rocketmail.com>
2021-01-26 16:43:14 -0500

    changes

---
+++ frontend/splash/product_slice.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductSlice = (props) => {
+    // debugger
+    return (
+        <ul className="product-list">
+            {props.products.map(product=> <li className="product-list-index">
+            <Link to={`/products/${product.id}`}>
+                <img className="product-image" src={product.photoUrl} />
+            </Link>
+            <p className="product-price">${product.price}</p>
+        </li>)}
+        </ul>
+    )
+}
+
+export default ProductSlice;
---
+++ frontend/splash/splash_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions/product_actions';
+import Splash from './splash';
+
+const mapStateToProps = (state, ownProps) => ({
+    products: Object.values(state.entities.products)
+}) 
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchProducts: () => dispatch(fetchProducts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Splash)
