0dac4e916ec8ae84ceb298999d986c647fb95616
Monica Liang <monicaliang@rocketmail.com>
2021-01-26 14:00:18 -0500

    edit render in Products Controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,7 +2,7 @@ class Api::ProductsController < ApplicationController
     def show 
         @product = Product.find_by(id: params[:id])
         # debugger
-        render :show
+        render "api/products/show"
     end
 
     def index 
