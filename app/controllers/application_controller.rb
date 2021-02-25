8a25a7561f88adab3d01dc71ce21bf582ff234ea
Monica Liang <monicaliang@rocketmail.com>
2021-02-25 17:31:01 -0500

    removed debuggers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,13 @@ class ApplicationController < ActionController::Base
     end
     
     def login(user)
-        # debugger
         user.reset_session_token!
         session[:session_token] = user.session_token
         @current_user = user
-        # debugger
     end
     
     def logout
         current_user.reset_session_token!
-        # debugger
         session[:session_token] = nil
         @current_user = nil
     end
