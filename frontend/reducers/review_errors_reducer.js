2c4d16706d310fc776348e401ebedd68d805ea78
Monica Liang <monicaliang@rocketmail.com>
2021-03-01 15:10:39 -0500

    added errors reducer for reviews

---
+++ frontend/reducers/review_errors_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_REVIEW, REMOVE_REVIEW, RECEIVE_ALL_REVIEWS, RECEIVE_REVIEW_ERRORS } from '../actions/review_actions';
+
+const reviewErrorsReducer = (state = [], action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_REVIEW:
+            return [];
+        case REMOVE_REVIEW: 
+            return [];
+        case RECEIVE_ALL_REVIEWS:
+            return [];
+        case RECEIVE_REVIEW_ERRORS: 
+            return action.errors
+        default: 
+            return state;
+    }
+}
+
+export default reviewErrorsReducer;
