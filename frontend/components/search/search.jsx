edab4a12e4889a8a7a8fffe77beec5d88dc28a0a
Monica Liang <monicaliang@rocketmail.com>
2021-03-11 15:48:48 -0500

    created search results component

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import SearchResults from './search_results';
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+    }
+
+    componentDidMount() {
+        this.props.fetchProducts();
+    }
+
+    update(type) {
+        return (e) => {
+            this.setState({ [type]: e.currentTarget.value })
+        }
+    }
+
     render() {
-        return null;
+        const { products } = this.props;
+        debugger
+        return(
+            <div>
+                <button className="search-button-signout"><img className="search-image"src={window.magnifying} /></button>
+                <input 
+                type="text" 
+                className="searchBar-signout" 
+                placeholder="Search for anything"
+                value={this.state.searchTerm}
+                onChange={this.update('searchTerm')}>
+                </input>
+            </div>
+        )
     }
 }
 
